a820924b8a18094a3f690a4cd294bf09d7a2c073
Krisdiyansyah <krisdiyansyah1@gmail.com>
2019-11-15 14:15:59 +0700

    Modul 9

---
+++ Modul09/Modul-9/Cookies/hapus_cookie.php
@@ -0,0 +1,14 @@
+<?php   
+// set masa berlakunya menjadi sejam yang lalu (sudah lewat)   
+setcookie("user", "", time() - 3600);   
+?>   
+
+<html>   
+<body>   
+	   
+	<?php   
+	echo "Cookie 'user' terhapus.";   
+	?>   
+   
+</body>   
+</html>
